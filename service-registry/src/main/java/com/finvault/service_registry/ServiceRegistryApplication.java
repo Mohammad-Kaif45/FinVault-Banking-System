46a6154b7cf6ccb269bd7d73957689fc422a8104
Kaif <kaifmumtajansari@gmail.com>
2026-01-19 11:35:03 +0530

    Initial Commit

---
+++ service-registry/src/main/java/com/finvault/service_registry/ServiceRegistryApplication.java
@@ -0,0 +1,15 @@
+package com.finvault.service_registry;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
+}
