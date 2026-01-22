bd0a8225b066c02e65de41bf76e8e1d888961a9f
Kaif <kaifmumtajansari@gmail.com>
2026-01-22 12:01:19 +0530

    Week 6 Day 2

--- api-gateway/src/main/java/com/finvault/api_gateway/filter/LoggingFilter.java
+++ api-gateway/src/main/java/com/finvault/api_gateway/filter/LoggingFilter.java
@@ -1,7 +1,5 @@
 package com.finvault.api_gateway.filter;
 
-
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
@@ -10,6 +8,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.UUID;
+
 @Component
 public class LoggingFilter implements GlobalFilter {
 
@@ -17,15 +17,20 @@ public class LoggingFilter implements GlobalFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        // 1. Log the Incoming Request
-        logger.info("--------------------------------------------------");
-        logger.info("🚪 GATEWAY ENTRY: Path request: {}", exchange.getRequest().getPath());
-        logger.info("📨 Method: {}", exchange.getRequest().getMethod());
+        // 1. Generate a unique "Luggage Tag" for this request
+        String traceId = UUID.randomUUID().toString();
 
-        // 2. Continue the Chain (Forward to Microservice)
+        // 2. Add the Trace ID to the Response Headers (So Postman sees it)
+        exchange.getResponse().getHeaders().add("X-Trace-Id", traceId);
+
+        // 3. Log the Entry with the Trace ID
+        logger.info("--------------------------------------------------");
+        logger.info("🚪 [Trace: {}] ENTRY: {} {}", traceId, exchange.getRequest().getMethod(), exchange.getRequest().getPath());
+
+        // 4. Continue the Chain
         return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-            // 3. Log the Outgoing Response (After Microservice is done)
-            logger.info("✅ GATEWAY EXIT: Response Status: {}", exchange.getResponse().getStatusCode());
+            // 5. Log the Exit with the SAME Trace ID
+            logger.info("✅ [Trace: {}] EXIT: Status: {}", traceId, exchange.getResponse().getStatusCode());
             logger.info("--------------------------------------------------");
         }));
     }
