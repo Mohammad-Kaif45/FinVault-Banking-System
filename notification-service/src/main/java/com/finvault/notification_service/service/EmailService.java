59311837064dbf367538031f77c788bd4cd6f4ed
Kaif <kaifmumtajansari@gmail.com>
2026-02-14 20:36:21 +0530

    Update

--- notification-service/src/main/java/com/finvault/notification_service/service/EmailService.java
+++ notification-service/src/main/java/com/finvault/notification_service/service/EmailService.java
@@ -1,4 +1,28 @@
 package com.finvault.notification_service.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String body) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom("your_email@gmail.com"); // 👈 Match config email
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
+
+            mailSender.send(message);
+            System.out.println("✅ Email Sent Successfully to " + to);
+        } catch (Exception e) {
+            System.err.println("❌ Error sending email: " + e.getMessage());
+        }
+    }
 }
