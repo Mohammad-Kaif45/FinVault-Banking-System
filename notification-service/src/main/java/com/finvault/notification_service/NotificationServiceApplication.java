00b8d11f086a26a11edf51eaaef3d908b063bc2c
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-05-08 19:23:15 +0530

    Complete readme File

--- notification-service/src/main/java/com/finvault/notification_service/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/finvault/notification_service/NotificationServiceApplication.java
@@ -11,7 +11,6 @@ public class NotificationServiceApplication {
 	// MAIN MEHTOD
 
 	public static void main(String[] args) {
-        //
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
 }
