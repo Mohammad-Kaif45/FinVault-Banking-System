74e6a34cebb4997ad7a7bda50a558199adda63b2
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-09-17 21:32:15 +0530

    Update in TransactionController.java

--- notification-service/src/main/java/com/finvault/notification_service/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/finvault/notification_service/NotificationServiceApplication.java
@@ -8,9 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class NotificationServiceApplication {
    // Notification Service application
-	// MAIN MEHTOD is the
-
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
+		// APP
 	}
 }
