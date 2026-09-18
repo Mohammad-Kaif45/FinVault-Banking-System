aed47f2103c6dcd39f0c51bf9c217bed117e456a
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-09-18 22:14:27 +0530

    Update in TransactionController.java

--- notification-service/src/main/java/com/finvault/notification_service/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/finvault/notification_service/NotificationServiceApplication.java
@@ -10,6 +10,6 @@ public class NotificationServiceApplication {
    // Notification Service application
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
-		// APP
+		// APP IS
 	}
 }
